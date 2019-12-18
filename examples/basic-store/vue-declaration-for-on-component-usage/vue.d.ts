b0620dbb6dac08c4b8fbbf88d3afab92b38fc70d
Jonah Hoff <>
2019-12-18 14:31:58 +0100

    used patch package to remove the vuex declaration

--- examples/basic-store/vue-declaration-for-on-component-usage/vue.d.ts
+++ examples/basic-store/vue-declaration-for-on-component-usage/vue.d.ts
@@ -1,17 +1,7 @@
-import Vue from "vue";
 import store from "../implementation";
-
-declare module "vue/types/options" {
-  interface ComponentOptions<V extends Vue> {
-    // currently required since otherwise at the constructor
-    // of vue a typescript error might occur
-    store?: any;
-  }
-}
 
 declare module "vue/types/vue" {
   interface Vue {
-    // this will
     $store: typeof store;
   }
 }
